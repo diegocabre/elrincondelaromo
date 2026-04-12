88e1080c8181c147c865358b7ee15296ae5d2f88
Diego Cabre <diegocabre@gmail.com>
2026-04-12 11:33:53 -0400

    fix: Remover import sin usar para arreglar compilacion en Vercel

--- src/components/home/HomeServices.tsx
+++ src/components/home/HomeServices.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { CheckCircle2, PlayCircle } from "lucide-react";
+import { CheckCircle2 } from "lucide-react";
 
 const fadeUp = {
   hidden: { opacity: 0, y: 30 },
